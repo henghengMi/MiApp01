352771f53b4b5e5f0acccd327db8f9cd92b7522a
heng <240668871@qq.com>
2016-05-17 11:42:51 +0800

    初始化项目

---
+++ MiBiBiBi/MiBiBiBi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MiBiBiBi
+//
+//  Created by YuanMiaoHeng on 16/5/17.
+//  Copyright © 2016年 Mi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MiBiBiBi/MiBiBiBi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MiBiBiBi
+//
+//  Created by YuanMiaoHeng on 16/5/17.
+//  Copyright © 2016年 Mi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
